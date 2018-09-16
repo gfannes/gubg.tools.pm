6edc2396345fabcaf6cbfed1623271cbd6f9ceca
Geert Fannes <geert.fannes@gmail.com>
2018-09-16 19:14:12 +0200

    wip

--- src/app/pit/Model.hpp
+++ src/app/pit/Model.hpp
@@ -46,7 +46,7 @@ namespace pit {
             xtree_.clear();
             //Recursive parsing of content
             MSS(parse_(xtree_.root(), range), std::cout << "Error: parsing failed" << std::endl);
-            MSS(range.empty(), std::cout << "Error: could not understand part \"" << range.str() << "\"" << std::endl);
+            MSS(range.empty(), std::cout << "Error: could not understand part \"" << range.str().substr(0, 10) << "\"" << std::endl);
             MSS_END();
         }
 
@@ -62,6 +62,7 @@ namespace pit {
         bool parse_(XTree::Node &parent, gubg::naft::Range &range)
         {
             MSS_BEGIN(bool, "");
+
             std::string tag;
             while (range.pop_tag(tag))
             {
@@ -77,12 +78,13 @@ namespace pit {
                     const auto &key = p.first;
                     const auto &value = p.second;
                     L(C(key)C(value));
+
                     if (false) {}
                     else if (key == "note") {node.note = value;}
                     else if (key == "todo")
                     {
                         node.todo.emplace();
-                        MSS(is_hours_(*node.todo, value) || is_army_(*node.todo, value));
+                        MSS(is_hours_(*node.todo, value) || is_days_(*node.todo, value) || is_army_(*node.todo, value));
                     }
                     else if (key == "done") {node.todo.emplace();}
                     else if (key == "deadline") {node.deadline = value;}
@@ -90,22 +92,32 @@ namespace pit {
                     else if (key == "should") {node.moscow = Moscow::Should;}
                     else if (key == "could") {node.moscow = Moscow::Could;}
                     else if (key == "wont") {node.moscow = Moscow::Wont;}
-                    else if (is_hours_(duration, key)) {node.duration = duration;}
-                    else if (is_army_(duration, key)) {node.duration = duration;}
+                    else if (key == "who") {}
+                    else if (key == "dep") {}
+                    else if (is_hours_(duration, key) || is_days_(duration, key) || is_army_(duration, key)) {node.duration = duration;}
+
+                    else if (key.empty())
+                    {
+                    }
+
                     else
                     {
                         MSS(false, std::cout << "Error: unknown attribute key \"" << key << "\"" << std::endl);
                     }
                 }
+
                 if (node.duration)
                     node.total_duration = *node.duration;
                 if (node.todo)
                     node.total_todo = *node.todo;
                 else if (node.duration)
                     node.total_todo = *node.duration;
+
                 gubg::naft::Range block;
                 if (range.pop_block(block))
+                {
                     MSS(parse_(node, block));
+                }
                 parent.total_duration += node.total_duration;
                 parent.total_todo += node.total_todo;
             }
@@ -121,6 +133,17 @@ namespace pit {
             MSS_Q(strange.pop_if('h'));
             MSS(strange.empty());
             duration.from_minutes(hours*60.0);
+            MSS_END();
+        }
+        static bool is_days_(gubg::Army &duration, const std::string &key)
+        {
+            MSS_BEGIN(bool);
+            gubg::Strange strange(key);
+            double days;
+            MSS_Q(strange.pop_float(days));
+            MSS_Q(strange.pop_if('d'));
+            MSS(strange.empty());
+            duration.from_minutes(days*8.0*60.0);
             MSS_END();
         }
         static bool is_army_(gubg::Army &duration, const std::string &key)
