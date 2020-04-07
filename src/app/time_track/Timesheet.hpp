19b2c1b679284be683a1e2fd8f805a20d90863d3
Geert Fannes <geert.fannes@gmail.com>
2020-04-07 03:05:07 +0200

    Changed lunchbreak to more realistic time

--- src/app/time_track/Timesheet.hpp
+++ src/app/time_track/Timesheet.hpp
@@ -63,8 +63,8 @@ namespace time_track {
 
             std::unique_ptr<Day> filter_from_day_;
 
-            const DayTime pause_begin_{12,0,0};
-            const DayTime pause_end_{12,30,0};
+            const DayTime pause_begin_{12,45,0};
+            const DayTime pause_end_{13,15,0};
             const Duration minimal_pause_() const {return pause_end_ - pause_begin_;}
     };
     inline std::ostream &operator<<(std::ostream &os, const Timesheet &ts)
